813151b2ada30848567d73add989dab0fbdffe43
Alastair Cooper <alastair.cooper@outlook.com>
2018-05-05 16:11:04 +0100

    Add 2nd search form

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,6 +22,25 @@ const Search = props => (
         </div>
       </div>
     </div>
+
+    <div className="row search-wrapper">
+      <div className="col">
+        <div className="form-group search-box">
+          <label htmlFor="customerName">Customer Name</label>
+          <div className="search-row">
+            <input
+              type="text"
+              id="customerId"
+              className="form-control"
+              placeholder="Customer Name"
+            />
+            <button className="btn btn-primary fn-submit-name">
+              Search names
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
