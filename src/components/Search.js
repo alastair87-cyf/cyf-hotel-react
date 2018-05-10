ff9d01bb21626870974690182a60405152bc6d8b
Alastair Cooper <alastair.cooper@outlook.com>
2018-05-10 01:12:21 +0100

    Re-positioned Search forms. Added Results.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,37 +6,40 @@ const Search = props => (
     <div className="page-header">
       <h4 className="text-left">Search Bookings</h4>
     </div>
-    <div className="row search-wrapper">
-      <div className="col">
-        <div className="form-group search-box">
-          <label htmlFor="customerId">Customer Id</label>
-          <div className="search-row">
-            <input
-              type="text"
-              id="customerId"
-              className="form-control"
-              placeholder="Customer Id"
-            />
-            <Button />
+
+    <div className="row">
+      <div className="search-wrapper">
+        <div className="col">
+          <div className="form-group search-box">
+            <label htmlFor="customerId">Customer Id</label>
+            <div className="search-row">
+              <input
+                type="text"
+                id="customerId"
+                className="form-control"
+                placeholder="Customer Id"
+              />
+              <Button />
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div className="row search-wrapper">
-      <div className="col">
-        <div className="form-group search-box">
-          <label htmlFor="customerName">Customer Name</label>
-          <div className="search-row">
-            <input
-              type="text"
-              id="customerId"
-              className="form-control"
-              placeholder="Customer Name"
-            />
-            <button className="btn btn-primary fn-submit-name">
-              Search names
-            </button>
+      <div className="search-wrapper">
+        <div className="col">
+          <div className="form-group search-box">
+            <label htmlFor="customerName">Customer Name</label>
+            <div className="search-row">
+              <input
+                type="text"
+                id="customerId"
+                className="form-control"
+                placeholder="Customer Name"
+              />
+              <button className="btn btn-primary fn-submit-name">
+                Search names
+              </button>
+            </div>
           </div>
         </div>
       </div>
