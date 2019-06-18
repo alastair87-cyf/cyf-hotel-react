11d1af37181a0fe6f801b95dd01d7d69a63d4adf
Alastair Cooper <alastair.cooper@outlook.com>
2019-06-18 12:30:17 +0100

    2. Extract the <header> in the src/App.js to be its own separate component called Heading. Make sure that you import and render the <Heading /> component within src/App.js.

--- src/Heading.js
+++ src/Heading.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Heading = () => {
-  return <header className="App-header">CYF Hotel</header>;
+  return (
+    <header className="App-header">
+      <span>
+        <img
+          className="App-logo"
+          src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        />
+      </span>
+      <p class="App-title">CYF Hotel</p>
+    </header>
+  );
 };
 
 export default Heading;
