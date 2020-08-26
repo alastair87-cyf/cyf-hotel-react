c9f48bc29eef835752e235c4e53fa056d2023e04
Alastair Cooper <alastair.cooper@outlook.com>
2020-08-26 14:31:13 +0100

    remove unused city prop from cards

--- src/App.js
+++ src/App.js
@@ -16,23 +16,20 @@ const App = () => {
               "https://i2-prod.glasgowlive.co.uk/incoming/article15679547.ece/ALTERNATES/s810/0_Met-Tower.jpg",
             alt: "Glasgow",
             href: "www.peoplemakeglasgow.com",
-            href_friendly: "peoplemakeglasgow.com",
-            city: ""
+            href_friendly: "peoplemakeglasgow.com"
           },
           {
             src:
               "https://www.aboutmanchester.co.uk/wp-content/uploads/2020/08/manchester-overtakes-edinburgh-as-second-most-popular-uk-tourist-destination-696x432.jpg",
             alt: "",
             href: "www.visitmanchester.com",
-            href_friendly: "visitmanchester.com",
-            city: "Manchester"
+            href_friendly: "visitmanchester.com"
           },
           {
             src: "https://media.timeout.com/images/100644443/image.jpg",
             alt: "",
             href: "www.visitlondon.com",
-            href_friendly: "visitlondon.com",
-            city: "London"
+            href_friendly: "visitlondon.com"
           }
         ]}
       />
